dfe8a9202874f5d6da246c4a7d2e13fe2701e12d
Yurii Chudinovych <yuriichudinovych93@gmail.com>
2022-08-24 15:27:59 +0300

    FT-01 add header's template

--- src/js/make-header.js
+++ src/js/make-header.js
@@ -1,6 +1,36 @@
+import { refs } from './common/refs';
+import logoIcon from '../images/logo-icon.svg';
+
 export default createHeader = () => {
-  const str = 'hello';
-  return str;
+  console.log(document.documentElement.clientWidth);
+  return (refs.header.innerHTML = markupHeaderMobile());
 };
 
-const markupHeader = () => {};
+const markupHeaderMobile = () => {
+  return `<div class="header-container container">
+    <div class="naw-wrapper">
+      <img
+        class="logo-icon"
+        src="${logoIcon}"
+        width="24"
+        height="24"
+        alt="logo icon"
+      />
+      <ul class="naw-list">
+        <li class="naw-list-item"><a class="naw-list-link" href="">Home</a></li>
+        <li class="naw-list-item">
+          <a class="naw-list-link" href="">my library</a>
+        </li>
+      </ul>
+    </div>
+
+    <form class="contact-form">
+      <label class="contact-form__field">
+        <input class="contact-form__input" type="text" name="name" placeholder = 'Movie search' />
+      </label>
+      <button class="modal-btn-create-bank" type="submit">Створити</button>
+    </form>
+  </div>`;
+};
+
+createHeader();
