0ca9ed5860c2a927bcde02e2dd751f710f489b7b
Yurii Chudinovych <yuriichudinovych93@gmail.com>
2022-08-24 16:20:40 +0300

    FT-01 add home header's styles

--- src/js/make-header.js
+++ src/js/make-header.js
@@ -1,5 +1,6 @@
 import { refs } from './common/refs';
-import logoIcon from '../images/logo-icon.svg';
+import logoIcon from '../images/header/logo-icon.svg';
+import search from '../images/header/search.svg';
 
 export default createHeader = () => {
   console.log(document.documentElement.clientWidth);
@@ -17,18 +18,24 @@ const markupHeaderMobile = () => {
         alt="logo icon"
       />
       <ul class="naw-list">
-        <li class="naw-list-item"><a class="naw-list-link" href="">Home</a></li>
+        <li class="naw-list-item"><a class="naw-list-link current" href="">Home</a></li>
         <li class="naw-list-item">
           <a class="naw-list-link" href="">my library</a>
         </li>
       </ul>
     </div>
 
-    <form class="contact-form">
-      <label class="contact-form__field">
-        <input class="contact-form__input" type="text" name="name" placeholder = 'Movie search' />
+    <form class="search-form">
+      <label class="search-form__field">
+        <input class="search-form__input" type="text" name="name" placeholder = 'Movie search' />
       </label>
-      <button class="modal-btn-create-bank" type="submit">Створити</button>
+      <button class="search-form__btn" type="submit"> <img
+        class="logo-icon"
+        src="${search}"
+        width="12"
+        height="12"
+        alt="logo icon"
+      /></button>
     </form>
   </div>`;
 };
