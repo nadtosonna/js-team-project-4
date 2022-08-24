2c59f0e0fb8c1fa09de6199739b02a5bcbed44d1
nadtosonna <prasolova.kateryna@gmail.com>
2022-08-25 00:48:24 +0300

    fetches update, fetch by id added

--- src/js/api/fetchMovies.js
+++ src/js/api/fetchMovies.js
@@ -3,42 +3,39 @@ import { API_KEY } from '../common/keys';
 
 const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie';
 const TRENDING_URL = 'https://api.themoviedb.org/3/trending/all/day';
+const MOVIE_ID_URL = 'https://api.themoviedb.org/3/movie/'
 
 export async function fetchTrendingMovies(page) {
-    const response = await axios.get(`${TRENDING_URL}?api_key=${API_KEY}&page=${page}`);
-    console.log(response.data);
-  return response.data;
+  try {
+    const { data } = await axios.get(`${TRENDING_URL}?api_key=${API_KEY}&page=${page}`);
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export async function fetchMovies(query, page) {
-    const response = await axios.get(`${SEARCH_URL}?api_key=${API_KEY}&language=en-US&include_adult=false`, {
+  try {
+  const { data } = await axios.get(`${SEARCH_URL}?api_key=${API_KEY}&language=en-US&include_adult=false`, {
         params: {
         query: query.length ? query : null,
         page,
       },
     },
     );
-    console.log(response.data);
-    return response.data;
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }  
 }
 
-// export default class trendingMovies {
-//     constructor() {
-//         this.page = 1;
-//         this.result;
-//     }
-//     async fetchTrendingMovies() {
-//         const response = await axios.get(`${TRENDING_URL}?api_key=${API_KEY}&page=${page}`);
-//         console.log(response);
-//         return response;
-//     }
-//     refreshPage() {
-//         this.page = 1;
-//     }
-//     increasePage() {
-//         this.page += 1;
-//     }
-//     decreasePage() {
-//         this.page += 1;
-//     }
-// }
+export async function fetchMoviesByID(id) {
+  try {
+    const { data } = await axios.get(`${MOVIE_ID_URL}${id}?api_key=${API_KEY}&language=en-US`);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
