b16040cd641f20efb25e34e54357924c1cbe01cf
nadtosonna <97362066+nadtosonna@users.noreply.github.com>
2022-08-24 10:40:28 +0000

    Deploying to gh-pages from @ nadtosonna/js-team-project-4@1d3b9854e7a1c1d4d7d030e3e4c4bd19b495995f 🚀

--- index.395f4c74.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=createHeader=function(){return"hello"};console.log(e())}();
-//# sourceMappingURL=index.395f4c74.js.map
--- index.76748bee.js
+++
@@ -1,2 +0,0 @@
-var e=createHeader=()=>"hello";console.log(e());
-//# sourceMappingURL=index.76748bee.js.map
