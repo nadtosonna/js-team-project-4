73214850078fcb846efa5023b619544c813a5029
Sarat862 <sarat862@gmail.com>
2022-08-30 18:06:52 +0300

    Update my-library-render.js

--- src/js/my-library-render.js
+++ src/js/my-library-render.js
@@ -4,7 +4,7 @@ import { markupEmptyTemplate } from './make-empty-template-my-library';
 const { moviesGallery, moviesGalleryContainer, library } = getRefs();
 
 function reduceWatcedFilms(array) {
-    return array.reduce((acc, film) => acc + getCardTemplate(film), "");
+    return array.reduce((acc, film) => acc + renderGalleryFromTemplate(film), "");
 }
 
 function renderWatchedFilms(array) {
@@ -62,11 +62,12 @@ const getCardTemplate = (movie) => {
     poster_path,
     vote_average,
     release_date,
-    genres
+    genres,
+    id
   } = movie;
 
   return `
-    <li class='movies-gallery__item'>
+    <li class='movies-gallery__item' data-id='${id}'>
       <div class='movies-gallery__img'>
         <img
           src='${
@@ -89,3 +90,53 @@ const getCardTemplate = (movie) => {
     </li>
   `;
 };
+
+function renderGalleryFromTemplate(data) {
+
+  moviesGallery.innerHTML = '';
+  
+  const {
+    title,
+    original_name,
+    original_title,
+    name,
+    poster_path,
+    release_date,
+    vote_average,
+    id,
+    genres
+  } = data;
+
+    return `
+    <li class='movies-gallery__item' data-id='${id}'>
+      <div class='movies-gallery__img'>
+        <img
+          src='${poster_path
+        ? `https://image.tmdb.org/t/p/w500${poster_path}`
+        : 'https://ik.imagekit.io/rqegzjddo/no-poster-avalible.png?ik-sdk-version=javascript-1.4.3&updatedAt=1661766934161'
+      }'
+          alt='${original_title || original_name}'
+          loading='lazy'
+          width='395'
+        />
+      </div>
+      <div class='movies-gallery__text'>
+        <p class='movies-gallery__title'>${title || name || original_title || original_name}</p>
+        <p class='movies-gallery__genre ellipsis'>
+          ${getGenres( genres )} | ${release_date?.split('-')[0] || 'Coming soon'}
+        </p>
+        <span class='movies-gallery__rating'>${Number(vote_average).toFixed(
+        1
+      )}</span>
+      </div>
+    </li>
+  `;
+}
+
+function getGenres(genres) {
+    const mapGenres = genres.map(genre => genre.name)
+    if (mapGenres.length > 2) {
+      return `${mapGenres.slice(0, 2).join(', ')}, Other`
+    }
+    return mapGenres.join(', ')
+}
